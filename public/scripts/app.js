eaec53e452836f894a9f6594a6fbc99a2d17d351
bdurb <brian@rpmgreenville.com>
2017-09-19 17:15:01 -0400

    added playground for reactDOM manipulation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -26,37 +26,5 @@ var template = React.createElement(
   )
 );
 
-var user = {
-  name: 'Andrew',
-  age: 26,
-  location: 'Funkytown'
-};
-function getLocation(location) {
-  if (location) {
-    return React.createElement(
-      'p',
-      null,
-      'Location: ',
-      location
-    );
-  }
-}
-var templateTwo = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    user.name ? user.name : 'Anonymous'
-  ),
-  user.age && user.age >= 18 && React.createElement(
-    'p',
-    null,
-    'Age: ',
-    user.age
-  ),
-  getLocation(user.location)
-);
 var appRoot = document.getElementById('app');
-
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,35 +1,17 @@
 
-var app = {
+const app = {
   title: 'Danjgo',
   subtitle: 'Unchained',
   options: ['One', 'Two']
 };
 
-var template = (
+const template = (
   <div>
-    <h1>{ app.title }</h1>
+    <h1>{app.title}</h1>
     {app.subtitle && <p>{app.subtitle}</p>}
     <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
   </div>
 );
 
-var user = {
-  name: 'Andrew',
-  age: 26,
-  location: 'Funkytown'
-};
-function getLocation(location) {
-  if (location) {
-    return <p>Location: {location}</p>;
-  }
-}
-var templateTwo = (
-  <div>
-    <h1>{user.name ? user.name : 'Anonymous'}</h1>
-    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-    {getLocation(user.location)}
-  </div>
-);
-var appRoot = document.getElementById('app');
-
+const appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
