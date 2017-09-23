28b48811189a4a64b24256ded9db312d482782d2
bdurb <brian@rpmgreenville.com>
2017-09-22 21:58:54 -0400

    added .babelrc and made changes to webpack

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
-import validator from 'validator';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log(validator.isEmail('test@gmail.com'));
+const template = <p>This is JSX from Webpack</p>
+
+ReactDOM.render(template, document.getElementById('app'));
