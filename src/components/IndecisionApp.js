92993c5a3c234992c9f396f0a1f3a7a7545bb3c4
bdurb <brian@rpmgreenville.com>
2017-09-23 15:10:36 -0400

    added babel plugin transform class properties

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -5,50 +5,21 @@ import Action from './Action';
 import Options from './Options';
 
 export default class IndecisionApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.handleDeleteOption = this.handleDeleteOption.bind(this);
-    this.state = {
-      options: []
-    };
-  }
-  componentDidMount() {
-    try {
-      const json = localStorage.getItem('options');
-      const options = JSON.parse(json);
-      if (options) {
-        this.setState(() => ({ options }));
-      }
-    } catch (e) {
+  state = {
+    options: []
+  };
 
-    }
-  } 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.options.length !== this.state.options.length) {
-      const json = JSON.stringify(this.state.options);
-      localStorage.setItem('options', json);
-    }
-  }
-  componentWillUnmount() {
-    console.log('componentwillunmount');
-  }
-  handleDeleteOptions() {
-    this.setState(() => ({options: [] }));
-  }
-  handleDeleteOption(optionToRemove) {
+  handleDeleteOption = (optionToRemove) => {
     this.setState((prevState) => ({
       options: prevState.options.filter((option) => optionToRemove !== option)
     }));
   }
-  handlePick() {
+  handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
     alert(option);
   }
-  handleAddOption(option) {
+  handleAddOption = (option) => {
     if (!option) {
       return 'Enter valid value to add item';
     } else if (this.state.options.indexOf(option) > -1) {
@@ -80,3 +51,28 @@ export default class IndecisionApp extends React.Component {
     );
   }
 }
+
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (e) {
+
+    }
+  } 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
+  }
+  componentWillUnmount() {
+    console.log('componentwillunmount');
+  }
+  handleDeleteOptions = () => {
+    this.setState(() => ({options: [] }));
+  }
+ 
