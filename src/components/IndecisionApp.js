298c4eecf140bce862006b42b3f9d5e2ef78592a
bdurb <brian@rpmgreenville.com>
2017-09-23 20:16:07 -0400

    added styling to all less the modal

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -49,10 +49,12 @@ export default class IndecisionApp extends React.Component {
     return (
       <div>
         <Header subtitle={subtitle}/>
+        <div className="container">
         <Action
           hasOptions={this.state.options.length > 0}
           handlePick={this.handlePick}
         />
+        <div className="widget">
         <Options
           options={this.state.options}
           handleDeleteOptions={this.handleDeleteOptions}
@@ -61,6 +63,8 @@ export default class IndecisionApp extends React.Component {
         <AddOption
           handleAddOption={this.handleAddOption}
         />
+        </div>
+        </div>
         <OptionModal
           selectedOption={this.state.selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
